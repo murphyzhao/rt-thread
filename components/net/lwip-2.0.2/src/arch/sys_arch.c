a5e7d7332dc909425de4b4980680d9cf44e35f1b
MurphyZhao <d2014zjt@163.com>
2017-10-25 17:00:08 +0800

    Modify the IP & GW & MSK ADDR configure mode as string mode, insted separation mode

--- components/net/lwip-2.0.2/src/arch/sys_arch.c
+++ components/net/lwip-2.0.2/src/arch/sys_arch.c
@@ -28,6 +28,7 @@
 #include "lwip/sio.h"
 #include "lwip/init.h"
 #include "lwip/dhcp.h"
+#include "lwip/inet.h"
 
 #include <string.h>
 
@@ -162,16 +163,20 @@ int lwip_system_init(void)
 
     /* set default ip address */
 #if !LWIP_DHCP
+    printf("Not open DHCP, Use default config...\n\n");
     if (netif_default != RT_NULL)
     {
+        printf("netif_default != RT_NULL\n");
         struct ip4_addr ipaddr, netmask, gw;
 
-        IP4_ADDR(&ipaddr, RT_LWIP_IPADDR0, RT_LWIP_IPADDR1, RT_LWIP_IPADDR2, RT_LWIP_IPADDR3);
-        IP4_ADDR(&gw, RT_LWIP_GWADDR0, RT_LWIP_GWADDR1, RT_LWIP_GWADDR2, RT_LWIP_GWADDR3);
-        IP4_ADDR(&netmask, RT_LWIP_MSKADDR0, RT_LWIP_MSKADDR1, RT_LWIP_MSKADDR2, RT_LWIP_MSKADDR3);
+        ipaddr.addr = inet_addr(RT_LWIP_IPADDR);
+        gw.addr = inet_addr(RT_LWIP_GWADDR);
+        netmask.addr = inet_addr(RT_LWIP_MSKADDR);
 
         netifapi_netif_set_addr(netif_default, &ipaddr, &netmask, &gw);
     }
+    else
+        printf("netif_default == RT_NULL\n");
 #endif
 	rt_kprintf("lwIP-%d.%d.%d initialized!\n", LWIP_VERSION_MAJOR, LWIP_VERSION_MINOR, LWIP_VERSION_REVISION);
 
